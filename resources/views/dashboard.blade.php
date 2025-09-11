a8b3c95866c5d11ece3c21be2e94edb82f5b2ef4
Winata <winatasukiman@gmail.com>
2025-09-11 15:38:11 +0700

    dashboard design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
                     </h1>
 
                     <p class="mt-6 text-gray-500 leading-relaxed">
-                        You are logged in! Your current role(s): 
+                        You are logged in!<br>Your current role(s): 
                         @foreach(auth()->user()->roles as $role)
                             <span class="inline-block bg-blue-100 text-blue-800 text-sm px-2 py-1 rounded mr-1">
                                 {{ $role->name }}
@@ -21,22 +21,18 @@
                         @endforeach
                     </p>
                     <p class="mt-6 text-gray-500 leading-relaxed">
-                        You are logged in! Your current permission(s): 
-                        @foreach(auth()->user()->permissions as $permission)
-                            <span class="inline-block bg-blue-100 text-blue-800 text-sm px-2 py-1 rounded mr-1">
-                                {{ $permission->name }}
-                            </span>
-                        @endforeach
+                        Your current permission(s):
+                        @if(auth()->user()->getAllPermissions()->count() > 0)
+                            @foreach(auth()->user()->getAllPermissions() as $permission)
+                                <span class="inline-block bg-blue-100 text-blue-800 text-sm px-2 py-1 rounded mr-1 mt-2">
+                                    {{ $permission->name }}
+                                </span>
+                            @endforeach
+                        @else
+                            <span class="inline-block bg-blue-100 text-blue-800 text-sm px-2 py-1 rounded mr-1">No permissions available.</span>
+                        @endif
                     </p>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
             </div>
         </div>
     </div>
