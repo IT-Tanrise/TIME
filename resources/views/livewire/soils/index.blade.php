1aad009146dd4cca824de62df9e6d577fe426662
Winata <winatasukiman@gmail.com>
2025-09-12 09:52:05 +0700

    after first checkout develop branch

--- resources/views/livewire/soils/index.blade.php
+++ resources/views/livewire/soils/index.blade.php
@@ -279,7 +279,7 @@
                                                             Edit Details
                                                         </button>
                                                         @endcan
-                                                        @can('costs.edit')
+                                                        @can('soil-costs.edit')
                                                         <button wire:click="showEditForm({{ $soil->id }}, 'costs')" 
                                                                 class="block px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left">
                                                             Manage Costs
